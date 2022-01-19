9e6a82d0a14c5ff8993357106f11e8722d0f19dd
marcel <marcelharder@protonmail.com>
2022-01-19 10:19:50 +0100

    after introducing https

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,6 +4,7 @@ import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AuthService } from './_services/AuthService.service';
 
@@ -15,7 +16,8 @@ import { AuthService } from './_services/AuthService.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
   ],
   providers: [
     AuthService,
