1248732cb414393f4dd98a2a2c5d6a64a16e60c2
marcel <marcelharder@protonmail.com>
2022-01-20 08:59:01 +0100

    test3

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,10 +7,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AuthService } from './_services/AuthService.service';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavComponent,
     
   ],
   imports: [
