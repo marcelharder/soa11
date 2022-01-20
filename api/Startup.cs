0073cb316d88228635f0933f03bc701116356baf
marcel <marcelharder@protonmail.com>
2022-01-20 16:20:50 +0100

    error handling in api is done

--- api/Startup.cs
+++ api/Startup.cs
@@ -7,6 +7,7 @@ using api.Data;
 using api.Extensions;
 using api.Interfaces;
 using api.Services;
+using API.Errors;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -53,9 +54,12 @@ namespace api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            app.UseMiddleware<ExceptionMiddleware>();
+            
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                //app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "api v1"));
             }
