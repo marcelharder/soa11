7dfee660fe0864ae100ecbe0ece4a060115229e0
marcel <marcelharder@protonmail.com>
2022-01-21 16:47:24 +0100

    struggling with testerrors page

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { HomeComponent } from './home/home.component';
 import { DetailsmainComponent } from './procedures/detailsmain/detailsmain.component';
 import { EuroscoredetailsComponent } from './procedures/euroscoredetails/euroscoredetails.component';
@@ -19,7 +20,13 @@ const routes: Routes = [
         path: 'euroscore/:id', outlet: 'details', component: EuroscoredetailsComponent
       }
 
-    ]}]
+    ]
+  
+  },
+  {path: 'errors', component: TestErrorsComponent},
+  {path: '**', component: HomeComponent, pathMatch: 'full'}
+
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
