ea285cabdaccc8ed8b1f218dd54738b36eb14d7a
marcel <marcelharder@protonmail.com>
2022-02-22 11:45:01 +0100

    about page added

--- client/src/app/about/about.component.ts
+++ client/src/app/about/about.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  contact = 0;
+  version = 1;
 
   constructor() { }
 
   ngOnInit(): void {
+    
   }
+  switchPanel(t: number){
+    switch (t) {
+      case 1: this.contact = 1; this.version = 0;break;
+      case 2: this.contact = 0; this.version = 1;break;
+    }
+  }
+
+
+  showContact(){if(this.contact === 1){return true;}}
+  showVersion(){if(this.version === 1){return true;}}
+
+  linkToCSD(){ window.location.href = "https://csd-website.azurewebsites.net";}
 
 }
