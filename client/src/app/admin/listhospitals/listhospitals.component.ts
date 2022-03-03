293a06d7ed48c51e02c26d7c560bdecfabbf2c97
marcel <marcelharder@protonmail.com>
2022-03-03 13:51:56 +0100

    busy with addHospital

--- client/src/app/admin/listhospitals/listhospitals.component.ts
+++ client/src/app/admin/listhospitals/listhospitals.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { dropItem } from 'src/app/_models/dropItem';
 import { Hospital } from 'src/app/_models/Hospital';
 import { HospitalService } from 'src/app/_services/hospital.service';
 
@@ -11,23 +13,59 @@ import { HospitalService } from 'src/app/_services/hospital.service';
 export class ListhospitalsComponent implements OnInit {
 
   hospitals: Array<Partial<Hospital>> = [];
+  selectedHospital: Hospital;
+  selectedCountry = 31;
+  listOfCountries:Array<dropItem> = [];
   editFlag = 0;
   addFlag = 0;
   listFlag = 1;
 
-  constructor(private hospitalService: HospitalService, private router: Router) { }
+  constructor(private hospitalService: HospitalService, private router: Router, private alertify: ToastrService) { }
 
   ngOnInit(): void {
-    this.hospitalService.getHospitalsInCountry(31);
+    this.loadDrops();
+    this.hospitalService.allHospitals().subscribe((next)=>{
+      this.hospitals = next;})
   }
 
-  Cancel() { }
+  loadDrops(){
+    this.listOfCountries.push({value:31,description:"Netherlands"});
+    this.listOfCountries.push({value:1,description:"US"});
+    this.listOfCountries.push({value:49,description:"UK"});
+    this.listOfCountries.push({value:63,description:"France"});
+    this.listOfCountries.push({value:37,description:"Germany"});
+    this.listOfCountries.push({value:39,description:"Italy"});
+    this.listOfCountries.push({value:966,description:"KSA"});
+  }
 
-  addHospital() { }
+  filterCountry(){
+    this.hospitalService.getHospitalsInCountry(this.selectedCountry).subscribe((next)=>{this.hospitals = next;})
+  }
 
-  deleteHospital() { }
+  showAdd(){if(this.addFlag === 1){return true;}}
+  showEdit(){if(this.editFlag === 1){return true;}}
 
-  editHospital() { }
+  Cancel() { this.router.navigate(['/']) }
+
+  addHospital() {
+    this.addFlag = 1;
+    this.editFlag = 0;
+   }
+
+   backFromAdd(ret: any){}
+
+  deleteHospital(ret: string) {
+    this.addFlag = 0;
+    this.editFlag = 0;
+    this.hospitalService.deleteHospital(ret).subscribe((next)=>{this.alertify.show("Hospital removed ...")})
+  }
+
+  editHospital() {
+    this.addFlag = 0;
+    this.editFlag = 1;
+  }
+
+  backFromEdit(ret: any){}
 
 
 
