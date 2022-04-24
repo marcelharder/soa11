5b28d74d27d4021d0754a7a848f84405c658e7df
marcel <marcelharder@protonmail.com>
2022-04-24 14:36:33 +0200

    wafels

--- client/src/app/premium/premium.component.ts
+++ client/src/app/premium/premium.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-premium',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PremiumComponent implements OnInit {
 
-  constructor() { }
+  check_free_1 = true;
+  check_free_2 = false;
+  check_free_3 = false;
+  check_free_4 = false;
+  check_free_5 = false;
+
+  check_premium_1 = true;
+  check_premium_2 = true;
+  check_premium_3 = true;
+  check_premium_4 = true;
+  check_premium_5 = true;
+
+  constructor(private alertify: ToastrService) { }
 
   ngOnInit() {
   }
+
+  btnClick(no: number){
+
+    switch(no){
+      case 1: this.alertify.show("button 1");break;
+
+    }
+
+  }
+
+  RequestPremium(){}
+  Cancel(){}
 
 }
