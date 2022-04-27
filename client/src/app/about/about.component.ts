6d33e56e57d410a6e85ea7a87b3cfd82148fbd27
marcel <marcelharder@protonmail.com>
2022-04-27 18:25:44 +0200

    credits added

--- client/src/app/about/about.component.ts
+++ client/src/app/about/about.component.ts
@@ -8,20 +8,35 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent implements OnInit {
   contact = 0;
   version = 1;
+  credits = 0;
 
   constructor() { }
 
   ngOnInit(): void {
     
   }
+
+  displayCredits(){
+   this.credits = 1;
+   this.contact = 0; 
+   this.version = 0;
+  }
+
+  displayStart(){
+    this.credits = 0;
+    this.contact = 0; 
+    this.version = 1;
+   }
+  
+
   switchPanel(t: number){
     switch (t) {
-      case 1: this.contact = 1; this.version = 0;break;
-      case 2: this.contact = 0; this.version = 1;break;
+      case 1: this.contact = 1; this.version = 0;this.credits = 0;break;
+      case 2: this.contact = 0; this.version = 1;this.credits = 0;break;
     }
   }
 
-
+  showCredits(){if(this.credits === 1){return true;}}
   showContact(){if(this.contact === 1){return true;}}
   showVersion(){if(this.version === 1){return true;}}
 
