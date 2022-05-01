110b37a4e7a82d50890338ca259c52a7e367499e
marcel <marcelharder@protonmail.com>
2022-05-01 11:29:03 +0200

    links added

--- client/src/app/documentation/documentation.component.ts
+++ client/src/app/documentation/documentation.component.ts
@@ -17,13 +17,13 @@ export class DocumentationComponent implements OnInit {
 
   linkToCSD(){ window.location.href = "https://csd-website.azurewebsites.net";}
 
-  linkToIntroduction(){}
+  linkToIntroduction(){window.location.href = "https://docs.google.com/document/d/15xb1ceMfCEqKYpYXL-0vomYm_gAtWhiy3bY-MWzB-0U/edit?usp=sharing"}
 
-  linkToFeatures(){}
+  linkToFeatures(){window.location.href = "https://docs.google.com/document/d/128XynzpsdFw5UU6CMi5NQCM_StVvHxrq1FWuFqZeoUM/edit?usp=sharing"}
 
-  linkToHistory(){}
+  linkToHistory(){window.location.href = "https://docs.google.com/document/d/1_R0pHK49oQ1t-F2Rf5uQmbgvDt2WnLSsK_IA69k2j6Y/edit?usp=sharing"}
 
-  linkToDeployment(){}
+  linkToDeployment(){window.location.href = "https://docs.google.com/document/d/16SP_tfH3onP3PKxXHrV7Z-9yQ9emN1FWExO5RmiTjaY/edit?usp=sharing"}
 
 
 }
--- client/src/app/tutorials/tutorials.component.ts
+++ client/src/app/tutorials/tutorials.component.ts
@@ -15,14 +15,14 @@ export class TutorialsComponent implements OnInit {
 
   linkToCSD(){ window.location.href = "https://csd-website.azurewebsites.net";}
 
-  linkToFeatures(){}
+  linkToGettingStarted(){window.location.href = "https://docs.google.com/document/d/127ifGSodq6aJ0TlUikeGByjv8wKpgYmRTXVmsRiEqMg/edit?usp=sharing"}
 
-  linkToConfiguration(){}
+  linkToConfiguration(){window.location.href = "https://docs.google.com/document/d/1vWHd47vurMdpIlmzEIG2RxPj300oX0_1gQp3e4TshJ0/edit?usp=sharing"}
 
-  linkToAddingProcedures(){}
+  linkToAddingProcedures(){window.location.href = "https://docs.google.com/document/d/1jkdBPY98mJhWnwNReY3LSEz0OkEv0_hogKPKCBP6LfY/edit?usp=sharing"}
 
-  linkToReporting(){}
+  linkToReporting(){window.location.href = "https://docs.google.com/document/d/13M-dMYMg-ikCmtFarYyxLs3S2JZY4YhsAZ2n7dLk2FA/edit?usp=sharing"}
 
-  linkToStatistics(){}
+  linkToStatistics(){window.location.href = "https://docs.google.com/document/d/1dHpsTpBH9RNKliCNt7kXsccqMeBKDbZNZoVrK2ghpM0/edit?usp=sharing"}
 
 }
